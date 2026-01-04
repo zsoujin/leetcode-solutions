407e6b5b4a48e8b4e8ff44563527d8ea08bf3d15
aknietmanas <manasakniet7@gmail.com>
2026-01-04 20:41:08 +0500

    Solved Two Numbers problem

---
+++ push.sh
@@ -0,0 +1,10 @@
+git add .
+git commit -m "Solved Two Numbers problem"
+git push
+
+# ./push.sh to push changes to remote repository
+# Remember to add a commit message between the quotes
+# Example: git commit -m "Added solution for problem XYZ"
+# Make sure you have the necessary permissions to push to the repository
+# and that you are on the correct branch before running this script.
+# You can run this script from the terminal
